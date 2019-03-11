d4a3877434432e9949460cafa3c75e827a1dae52
Chad Tindel <chad@tindel.net>
2019-03-11 13:23:06 +0000

    Fixed python libraries

--- packer/scripts/ubuntu/install_utils.sh
+++ packer/scripts/ubuntu/install_utils.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-apt-get install -y zip nmap sysstat jq zlib1g zlib1g-dev
+apt-get install -y zip nmap sysstat jq 
+
+# These are necessary for building python from source
+apt-get install -y zlib1g zlib1g-dev libffi-dev libssl-dev libreadline-dev libbz2-dev libsqlite-dev
