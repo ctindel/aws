f02f116ecaff534f7347ae47fcd462d117533fbe
Chad Tindel <chad@tindel.net>
2019-03-11 21:07:23 +0000

    Python updates

--- packer/scripts/ubuntu/install_utils.sh
+++ packer/scripts/ubuntu/install_utils.sh
@@ -2,5 +2,5 @@
 
 apt-get install -y zip nmap sysstat jq 
 
-# These are necessary for building python from source
-apt-get install -y zlib1g zlib1g-dev libffi-dev libssl-dev libreadline-dev libbz2-dev libsqlite-dev
+# These are necessary for building python from source with pyenv
+apt-get install -y zlib1g zlib1g-dev libffi-dev libssl-dev libreadline-dev libbz2-dev libsqlite3-dev
